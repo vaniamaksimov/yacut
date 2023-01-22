57ef81454a2fdc305d0577f4b49b3bae281f9442
vaniamaksimov <vaniamaksimov@gmail.com>
2023-01-22 17:46:44 +0400

    add default values to config

--- settings.py
+++ settings.py
@@ -11,6 +11,6 @@ MAX_LENGTH = 6
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'a606fc83e97b0e422ed95c63b9f1e07415741dedaba70a1d77d627ea8f913f61')
